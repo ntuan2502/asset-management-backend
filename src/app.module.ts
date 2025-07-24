91a5b2a82ad63cbb8dfc0d16a92e8a2e6f996ec9
Tuan Nguyen <ntuan.2502@gmail.com>
2025-07-24 14:17:57 +0700

    feat(graphql): configure Apollo Server with local landing page plugin and auto-generated schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      playground: false,
+    }),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
